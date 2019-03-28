fd4359d945474748e065773263846f34903b0bfc
Nate <nate@txgisci.evans.txstate.edu>
2019-03-28 00:59:05 +0000

    Testing for dev branch.

--- ilbal2.py
+++ ilbal2.py
@@ -36,20 +36,20 @@ def segment(filename):
         rout = dp.segmentation(model=slic, params=slic_params, src=src,
                                modal_radius=3)
 #        ##### temporary
-#        vout = dp.vectorize(image=rout, transform=src.transform, crs=src.crs)
+        vout = dp.vectorize(image=rout, transform=src.transform, crs=src.crs)
 #        vout.to_file("output/original_segs.shp")
 #        #####
 
         # Region Agency Graph to merge segments
-        orig = dp.bsq_to_bip(src.read([1, 2, 3], masked=True))
-        labels = (dp.bsq_to_bip(rout))[:, :, 0]
+#        orig = dp.bsq_to_bip(src.read([1, 2, 3], masked=True))
+#        labels = (dp.bsq_to_bip(rout))[:, :, 0]
 
-        rag = graph.rag_mean_color(orig, labels, mode='similarity')
-        rout = graph.cut_normalized(labels, rag)
+#        rag = graph.rag_mean_color(orig, labels, mode='similarity')
+#        rout = graph.cut_normalized(labels, rag)
 
         # Vectorize the RAG segments
-        rout = dp.bip_to_bsq(rout[:, :, np.newaxis])
-        vout = dp.vectorize(image=rout, transform=src.transform, crs=src.crs)
+#        rout = dp.bip_to_bsq(rout[:, :, np.newaxis])
+#        vout = dp.vectorize(image=rout, transform=src.transform, crs=src.crs)
 
         # Add spectral properties.
         vout = dp.add_zonal_properties(src=src, bands=[1, 2, 3],
@@ -72,7 +72,7 @@ def segment(filename):
         vout = dp.add_zonal_properties(image=edges, band_names=['sobel'],
                                        stats=['mean','min','max','std'],
                                        transform=src.transform, gdf=vout)
-        
+
         ###################
         # Temporary code to write to vector and raster formats...
         # for checking output. Akin to using print statements to debug.
@@ -85,7 +85,7 @@ def segment(filename):
         return vout
 
 
-def train(image_list):    
+def train(image_list):
     # build training Geodataframe
     vout = gpd.GeoDataFrame()
     for i, image in enumerate(image_list):
@@ -99,7 +99,7 @@ def train(image_list):
 
 def doWork(list, q):
     output = train(list)
-    
+
     q.put(output)
 
 
@@ -107,21 +107,21 @@ if __name__ == "__main__":
     import os
     if not os.path.exists("output"):
         os.makedirs("output")
-    
+
     ##### Set the location #####
-    location = "local" # "local" or "txgisci"
+    location = "txgisci" # "local" or "txgisci"
     if location == "local":
         training_path = "/home/nate/Documents/Research/Guatemala/training/"
     else:
         training_path = "/data1/guatemala/training/"
-        
+
     image_list = glob(training_path + "training_new_IMG_*.tif")
-    
+
     ##### Set the number of cores #####
     cores = 3
     partition = []
-    image_list = image_list[:4]
-    size = ceil(len(image_list) / cores)    
+    image_list = image_list[:3]
+    size = ceil(len(image_list) / cores)
     for id, core in enumerate(range(cores)):
         partition.append(image_list[id * size:(id + 1) * size])
 
@@ -129,17 +129,17 @@ if __name__ == "__main__":
 
     vout = gpd.GeoDataFrame()
     for part in partition:
-        vout = vout.append(train(part)) 
+        vout = vout.append(train(part))
 
     vout.to_file("output/full_training.shp")
 
     endTime = time.time()
     #calculate the total time it took to complete the work
     workTime =  endTime - startTime
-     
+
     #print results
     print("The job took " + str(workTime) + " seconds to complete")
-    
+
 #    #mark the start time
 #    startTime = time.time()
 #    #create a Queue to share results
